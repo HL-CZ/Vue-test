bd4c4ffe809c997c39caa97dd061f8d7829e57c1
HL-CZ <145472767+HL-CZ@users.noreply.github.com>
2026-03-17 18:15:43 +0000

    Remove Firebase config from main.js
    Removed sensitive Firebase configuration details.

--- src/main.js
+++ src/main.js
@@ -9,13 +9,7 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, connectAuthEmulator } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAZL3tHlAkTvRCvTVXb_3hSr2h5OUMuThI",
-  authDomain: "chat-game-b43c0.firebaseapp.com",
-  databaseURL: "https://chat-game-b43c0-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "chat-game-b43c0",
-  storageBucket: "chat-game-b43c0.firebasestorage.app",
-  messagingSenderId: "541968072167",
-  appId: "1:541968072167:web:67620bff6023c31d465142"
+ 
 };
 
 initializeApp(firebaseConfig)
